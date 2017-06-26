5ad6622dd6f693e9c576e64227849425b22ecd60
Shreve-La <shrevela@gmail.com>
2017-06-26 18:31:43 -0400

    added input handler, uppercase, lowercase

--- WordEffectsV2/main.m
+++ WordEffectsV2/main.m
@@ -10,8 +10,53 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BOOL gameOn = YES;
+        while (gameOn) {
+        
+#pragma Input Handler
+        // 255 unit long array of characters
+        char inputChars[255];
+        
+        printf("Input a string: ");
+        // limit input to max 255 characters
+        fgets(inputChars, 255, stdin);
+        
+        // print as a c string
+        printf("Your string is %s\n", inputChars);
+        
+        // convert char array to an NSString object
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        
+        // print NSString object
+        NSLog(@"Input was: %@", inputString);
+        
+        // print NSString memory address
+            NSLog(@"Memory address was: %p", inputString);
+            
+        // Make the string LOUDER
+            NSString *capitalize = inputString.uppercaseString;
+            NSLog(@"All Caps: %@. Memory Address: %p", capitalize, capitalize);
+        
+        // Make the string lowercase
+            NSString *lowercase = inputString.lowercaseString;
+            NSLog(@"Lowercase: %@. MemoryAddress: %p", lowercase, lowercase);
+            
+            
+
+        
+        
+        
+            
+            
+            
+            
+            
+        
+        }
+        
+        
+        
+        
     }
     return 0;
 }
